218f0d96973ef832033ee92fc26c630e75df3d31
surendrailla36 <167479442+surendrailla36@users.noreply.github.com>
2025-04-21 19:34:34 -0500

    Code refactored

--- Calculator.java
+++ Calculator.java
@@ -7,10 +7,10 @@ public class Calculator {
         double num1, num2, result;
         char operator;
         
-        System.out.print("Enter the 1st number: ");
+        System.out.print("Enter the first number: ");
         num1 = input.nextDouble();
         
-        System.out.print("Enter the 2nd number: ");
+        System.out.print("Enter the second number: ");
         num2 = input.nextDouble();
         
         System.out.print("Enter an operator (+, -, *, /): ");
